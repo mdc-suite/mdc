be5ee7979270308d6067701dc05fdd529a5b8eca
dmadronal <dmadronalquin@uniss.it>
2020-10-30 11:31:38 +0100

    Initialize variables in MDCBackend. Changed some HashMap to LinkedHashMap to have ordered maps where needed

--- eclipse/plugins/it.mdc.tool/src/it/mdc/tool/core/multiDataflowGenerator/EmpiricMerger.java
+++ eclipse/plugins/it.mdc.tool/src/it/mdc/tool/core/multiDataflowGenerator/EmpiricMerger.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -141,7 +142,7 @@ public class EmpiricMerger extends Merger {
 		sboxActorManager = new SboxActorManager();
 		//sboxLutManager = new SboxLutManager();
 		actorManager = new ActorManager();
-		networksInstances = new HashMap<String,Set<String>>();
+		networksInstances = new LinkedHashMap<String,Set<String>>();
 		//networkVertexMap = new HashMap<String,Map<String,String>>();
 	}
 
